1908d22d09808cbb50b14615a2def49f2432ec4e
Kevin <kevinabram1000@gmail.com>
2019-09-01 22:48:18 +0700

    Initial Commit

---
+++ MC3-LeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MC3-LeatherApp
+//
+//  Created by Kevin Abram on 01/09/19.
+//  Copyright © 2019 Kevin Abram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
