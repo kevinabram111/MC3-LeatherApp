061925c3fed6f2debd0c0d959ec2135b5740564f
Kevin <kevinabram1000@gmail.com>
2019-09-02 12:17:14 +0700

    commit commit

---
+++ MC3-LeatherApp/Parts/SectionEdit/SectionButton.swift
@@ -0,0 +1,27 @@
+//
+//  SectionButton.swift
+//  MC3-LeatherApp
+//
+//  Created by Kevin Abram on 02/09/19.
+//  Copyright © 2019 Kevin Abram. All rights reserved.
+//
+
+import UIKit
+
+class SectionButton: UIButton {
+    
+    override func awakeFromNib() {
+        self.layer.borderColor = UIColor.white.cgColor
+        self.layer.borderWidth = 1
+        self.backgroundColor = .brown
+        self.setTitle("Add Detail Photo", for: .normal)
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
